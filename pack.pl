1a8ec7434d854f44893a16c468f6fbacfa1ee436
Kilian Evang <noreply@texttheater.net>
2014-05-20 20:21:40 +0200

    declared attempt to use GitHub releases as a pack repository failed

--- pack.pl
+++ pack.pl
@@ -1,5 +1,4 @@
 name(pinyin).
 title('Parsing and generation of Hanyu Pinyin').
-version('0.1.1').
+version('0.1.2').
 author('Kilian Evang', 'kilian.evang@gmail.com').
-download('https://github.com/texttheater/pinyin/releases')
