807f374ddf31b1f6d17eda092be2cdaa67408bc1
Kilian Evang <noreply@texttheater.net>
2014-05-21 16:23:46 +0200

    bumped version number

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(pinyin).
 title('Parsing and generation of Hanyu Pinyin').
-version('0.1.4').
+version('0.1.5').
 author('Kilian Evang', 'kilian.evang@gmail.com').
 download('https://github.com/texttheater/pinyin.git').
 home('https://github.com/texttheater/pinyin').
